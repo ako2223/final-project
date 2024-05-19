ad78d5a2febce6a92c4e1bf1c3654e60adc1297a
ako2223 <akoval22@proton.me>
2024-05-19 13:49:24 +0600

    Nav icons style

--- src/components/Navigation/Navgation.jsx
+++ src/components/Navigation/Navgation.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import style from "./Navigation.module.scss";
 import { NavLink } from "react-router-dom";
+import { CgSmartHomeCooker } from "react-icons/cg";
 
 function Navgation() {
   return (
@@ -15,7 +16,7 @@ function Navgation() {
         <NavLink className={style.link} to={'/catalog'}>Интересное</NavLink>
                         <NavLink className={style.link} to={'/shop'}> <img className={style.icon__img} src={require('../../assets/img/anbunet.png')} alt="anbunet logo" />Сообщения</NavLink>
                         <NavLink className={style.link} to={'/blog'}>Уведомления</NavLink>
-                        <NavLink className={style.link} to={'/news'}>Новый пост</NavLink>
+                        <NavLink className={style.link} to={'/news'}><div style={{display: "flex", justifyContent: "center", alignItems: "center"}}><CgSmartHomeCooker size={20} /> Новый пост</div></NavLink>
                         <NavLink className={style.link} to={'/contacts'}>Профиль</NavLink>
                         <NavLink className={style.link} to={'/login'}>Вход</NavLink>
                         <NavLink className={style.link} to={'/registration'}>Регистрация</NavLink>
