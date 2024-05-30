0e687728f63f4b3944dce11323d27af7c5fbcf37
ako2223 <akoval22@proton.me>
2024-05-30 15:04:07 +0600

    New color scheme, proposed

--- src/components/Navigation/Navgation.jsx
+++ src/components/Navigation/Navgation.jsx
@@ -11,18 +11,18 @@ function Navgation() {
     
       <div className={style.navigate_links}>
         <NavLink className={style.link} to={"/"}>
-        <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}><FcHome size={20} /><span className={style.hidden2}>Главная</span></div>
+        <div style={{display: "flex", justifyContent: "center", alignItems: "center",gap: "5px"}}><FcHome size={20} /><span className={style.hidden2}>Главная</span></div>
         </NavLink>
         <NavLink className={style.link} to={"/about_us"}>
-        <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}><FcSearch size={20} /> Поиск</div>
+        <div style={{display: "flex", justifyContent: "center", alignItems: "center",gap: "5px"}}><FcSearch size={20} /> Поиск</div>
         </NavLink>
-        <NavLink className={style.link} to={'/catalog'}><div style={{display: "flex", justifyContent: "center", alignItems: "center"}}><FcBinoculars size={20} /> Интересное</div></NavLink>
-                        <NavLink className={style.link} to={'/shop'}> <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}><FcAbout size={20} /> Сообщения</div></NavLink>
-                        <NavLink className={style.link} to={'/blog'}><div style={{display: "flex", justifyContent: "center", alignItems: "center"}}><FcLike size={20} /> Уведомления</div></NavLink>
-                        <NavLink className={style.link} to={'/newpost'}><div style={{display: "flex", justifyContent: "center", alignItems: "center"}}><FcMultipleCameras size={20} /> Новый пост</div></NavLink>
-                        <NavLink className={style.link} to={'/contacts'}><div style={{display: "flex", justifyContent: "center", alignItems: "center"}}><FcButtingIn size={20} /> Профиль</div></NavLink>
-                        <NavLink className={style.link} to={'/login'}><div style={{display: "flex", justifyContent: "center", alignItems: "center"}}><AiOutlineLogin size={20} /> Вход</div></NavLink>
-                        <NavLink className={style.link} to={'/registration'}><div style={{display: "flex", justifyContent: "center", alignItems: "center"}}><FcRegisteredTrademark size={20} /> Регистрация</div></NavLink>
+        <NavLink className={style.link} to={'/catalog'}><div style={{display: "flex", justifyContent: "center", alignItems: "center",gap: "5px"}}><FcBinoculars size={20} /> Интересное</div></NavLink>
+                        <NavLink className={style.link} to={'/shop'}> <div style={{display: "flex", justifyContent: "center", alignItems: "center",gap: "5px"}}><FcAbout size={20} /> Сообщения</div></NavLink>
+                        <NavLink className={style.link} to={'/blog'}><div style={{display: "flex", justifyContent: "center", alignItems: "center",gap: "5px"}}><FcLike size={20} /> Уведомления</div></NavLink>
+                        <NavLink className={style.link} to={'/newpost'}><div style={{display: "flex", justifyContent: "center", alignItems: "center",gap: "5px"}}><FcMultipleCameras size={20} /> Новый пост</div></NavLink>
+                        <NavLink className={style.link} to={'/contacts'}><div style={{display: "flex", justifyContent: "center", alignItems: "center",gap: "5px"}}><FcButtingIn size={20} /> Профиль</div></NavLink>
+                        <NavLink className={style.link} to={'/login'}><div style={{display: "flex", justifyContent: "center", alignItems: "center",gap: "5px"}}><AiOutlineLogin size={20} /> Вход</div></NavLink>
+                        <NavLink className={style.link} to={'/registration'}><div style={{display: "flex", justifyContent: "center", alignItems: "center",gap: "5px"}}><FcRegisteredTrademark size={20} /> Регистрация</div></NavLink>
       </div>
     
   );
